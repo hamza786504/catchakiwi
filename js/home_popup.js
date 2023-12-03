cf6662b69bf1d0e42bce2dad4d0aae9a9a1eb86d
hamza <hamza786504@gmail.com>
2023-12-03 19:21:16 +0500

    desktop part ready

--- js/home_popup.js
+++ js/home_popup.js
@@ -8,4 +8,17 @@ $(function () {
       const dataSection = $(this).data('section');
       $("#" + dataSection).addClass("active");
     });
+
+
+    $('.accordion-button').click(function () {
+      var accordionItem = $(this).closest('.accordion-item');
+      $('.accordion-item').removeClass("open");
+      accordionItem.addClass("open");
+      var icon = accordionItem.hasClass('collapsed') ? '-' : '+';
+
+      // Toggle the collapsed class and switch the icon
+      accordionItem.toggleClass('collapsed');
+      $(this).find('.accordion-button::before').text(icon);
+  });
+
   });
