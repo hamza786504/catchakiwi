12ce93997e46ac885ecdcb7e97cd9734e80b487b
hamza <hamza786504@gmail.com>
2023-12-04 15:59:08 +0500

    new commit

--- js/home_popup.js
+++ js/home_popup.js
@@ -1,24 +1,22 @@
 $(function () {
-    $(".sidebar_navigation ul li a").click(function (e) {
-      e.preventDefault();
-      $(".sidebar_navigation ul li a").removeClass("active");
-      $(this).addClass("active");
+  $(".sidebar_navigation ul li a").click(function (e) {
+    e.preventDefault();
+    $(".sidebar_navigation ul li a").removeClass("active");
+    $(this).addClass("active");
 
-      $(".sections .section").removeClass("active");
-      const dataSection = $(this).data('section');
-      $("#" + dataSection).addClass("active");
-    });
-
-
-    $('.accordion-button').click(function () {
-      var accordionItem = $(this).closest('.accordion-item');
-      $('.accordion-item').removeClass("open");
-      accordionItem.addClass("open");
-      var icon = accordionItem.hasClass('collapsed') ? '-' : '+';
-
-      // Toggle the collapsed class and switch the icon
-      accordionItem.toggleClass('collapsed');
-      $(this).find('.accordion-button::before').text(icon);
+    $(".sections .section").removeClass("active");
+    const dataSection = $(this).data("section");
+    $("#" + dataSection).addClass("active");
   });
 
+  $(".accordion-button").click(function () {
+    var accordionItem = $(this).closest(".accordion-item");
+    $(".accordion-item").removeClass("open");
+    accordionItem.addClass("open");
+    var icon = accordionItem.hasClass("collapsed") ? "-" : "+";
+
+    // Toggle the collapsed class and switch the icon
+    accordionItem.toggleClass("collapsed");
+    $(this).find(".accordion-button::before").text(icon);
   });
+});
